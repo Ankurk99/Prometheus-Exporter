33be5d93aa67bb2ccbe5e693ba0ff8a6b77c8f4a
Ankur Kothiwal <ankur.kothiwal99@gmail.com>
2021-07-01 16:24:01 +0530

    add Prometheus metrics and grafana dashboard" Signed-off-by: Ankur Kothiwal <ankur.kothiwal@accuknox.com>

--- test1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker image build . -t kubearmor-prometheus-client:$1
-docker tag kubearmor-prometheus-client:$1 localhost:5000/kubearmor-prometheus-client:$1
-
-docker push localhost:5000/kubearmor-prometheus-client:$1
-
-
